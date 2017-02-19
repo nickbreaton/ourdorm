e062ce5dbac55ea481ca0ac65eedcab241fdd829
Nick Breaton <nick@breaton.com>
2017-02-18 21:20:32 -0500

    Add harp and adjust layout

--- src/components/CalendarEvents.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  position: absolute;
-  width: 100%;
-  top: 30px;
-  padding: 5px;
-  box-sizing: border-box;
-`;
--- src/stores/index.js
+++
@@ -1,5 +0,0 @@
-import { calendar } from './Calendar';
-
-export default {
-  calendar
-};
