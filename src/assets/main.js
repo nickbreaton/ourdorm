34b9396410480dd4c76661b6c5887fcb4a79e2c8
nickbreaton <nick@breaton.com>
2017-03-21 21:19:00 -0400

    Preliminary slider animation

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,3 +9,26 @@ $('form').submit(function (event) {
   // clear all form fields
   event.target.reset();
 });
+
+var page = 0;
+
+$('.arrow').click(function (event) {
+  var className = event.target.className;
+  var direction = (className.indexOf('right') > -1) ? 1 : -1;
+  updateSlider(direction);
+});
+
+function updateSlider(direction) {
+  // update current page index
+  page += direction;
+  // contain index in bounds
+  page = (page > 2) ? 0 : page;
+  page = (page < 0) ? 2 : page;
+  // change class on bullet
+  $('.slider .bullet').each(function (i, bullet) {
+    var action = (page === i) ? 'addClass' : 'removeClass';
+    $(bullet)[action]('active');
+  });
+  // tmp
+  $('.browser').toggleClass('active');
+}
