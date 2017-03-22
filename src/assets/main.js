a55165c197110e6bb94303c44fb5310dd4e6988f
nickbreaton <nick@breaton.com>
2017-03-22 01:22:44 -0400

    Implement bullet functionality on slider

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,5 @@
 ---
 ---
-
 $('form').submit(function (event) {
   // prevent GET request
   event.preventDefault();
@@ -11,61 +10,91 @@ $('form').submit(function (event) {
 });
 
 // PRODUCT
+$(function product() {
+  var page = 0;
+  var total = $('.browser').length;
+  var timeout;
 
-var page = 0;
-var total = $('.browser').length;
-var timeout;
+  // start on current page
+  $('.browser').css('left', '-150%');
+  $('.browser').eq(page).css('left', '0');
 
-// start on current page
-$('.browser').css('left', '-150%');
-$('.browser').eq(page).css('left', '0');
-
-// create bullets
-for (var i = 0; i < total; i++) {
-  $('.slider').append('<div class="bullet"></div>');
-}
-
-// activate current bullet
-$('.slider .bullet').eq(page).addClass('active');
-
-// start slider change timeout
-automateSlider();
-
-// list for arrow click events
-$('.arrow').click(function (event) {
-  var className = event.target.className;
-  var direction = (className.indexOf('right') > -1) ? 1 : -1;
-  updateSlider(direction);
-});
-
-function updateSlider(direction) {
-  // do not update if already updating
-  if ($('.browser').is(':animated')) return;
-  // save previous index
-  var previous = page;
-  // update current page index
-  page += direction;
-  // contain index in bounds
-  page = (page > total - 1) ? 0 : page;
-  page = (page < 0) ? total - 1 : page;
-  // change class on bullet
-  $('.slider .bullet').each(function (i, bullet) {
-    var action = (page === i) ? 'addClass' : 'removeClass';
-    $(bullet)[action]('active');
-  });
-  // animate browser frames
-  $('.browser').eq(page).css('left', direction * -1 * 150 + '%');
-  $('.browser').eq(page).animate({ left: '0' }, 500);
-  $('.browser').eq(previous).animate({ left: direction * 150 + '%' }, 500);
-  // clear previous timeout, set new timeout
-  if (timeout) {
-    clearTimeout(timeout);
-    automateSlider();
+  // create bullets
+  for (var i = 0; i < total; i++) {
+    $('.slider').append('<div class="bullet"></div>');
   }
-}
 
-function automateSlider() {
-  timeout = setTimeout(function () {
-    updateSlider(1);
-  }, 10000);
-}
+  // activate current bullet
+  $('.slider .bullet').eq(page).addClass('active');
+
+  // start slider change timeout
+  automate();
+
+  // list for arrow click events
+  $('.arrow').click(function (event) {
+    var className = event.target.className;
+    var direction = (className.indexOf('right') > -1) ? 1 : -1;
+    next(direction);
+  });
+
+  // determine which page to jump to
+  $('.slider .bullet').click(function () {
+    var node = this;
+    var index = -1;
+    $('.slider .bullet').each(function (i, bullet) {
+      if (node === bullet) index = i;
+    });
+    // do nothing if already at page
+    if (index === page) return;
+    // otherwise, jump to that page
+    to(index);
+  });
+
+  function next(direction) {
+    // do not update if already updating
+    if ($('.browser').is(':animated')) return;
+    // save previous index
+    var previous = page;
+    // update current page index
+    page += direction;
+    // update DOM
+    update(previous, direction);
+  }
+
+  function to(number) {
+    // do not update if already updating
+    if ($('.browser').is(':animated')) return;
+    // save previous index
+    var previous = page;
+    // set page to specified number
+    page = number;
+    // update DOM
+    update(previous, 1);
+  }
+
+  function update(previous, direction) {
+    // contain index in bounds
+    page = (page > total - 1) ? 0 : page;
+    page = (page < 0) ? total - 1 : page;
+    // change class on bullet
+    $('.slider .bullet').each(function (i, bullet) {
+      var action = (page === i) ? 'addClass' : 'removeClass';
+      $(bullet)[action]('active');
+    });
+    // animate browser frames
+    $('.browser').eq(page).css('left', direction * -1 * 150 + '%');
+    $('.browser').eq(page).animate({ left: '0' }, 500);
+    $('.browser').eq(previous).animate({ left: direction * 150 + '%' }, 500);
+    // clear previous timeout, set new timeout
+    if (timeout) {
+      clearTimeout(timeout);
+      automate();
+    }
+  }
+
+  function automate() {
+    timeout = setTimeout(function () {
+      next(1);
+    }, 10000);
+  }
+});
